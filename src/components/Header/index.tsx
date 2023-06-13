7b8505b1c0256e8897272e70c319813fed75d299
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-13 16:59:25 -0300

    Construcao do Modal e seus estilos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import * as Dialog from '@radix-ui/react-dialog';
 // Contexto do modal
 
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
   return (
@@ -15,14 +16,7 @@ export function Header() {
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-            </Dialog.Content>
-
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
