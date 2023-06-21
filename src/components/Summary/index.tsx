1fd706e7a58443da38143ebadf4d724013da4c0c
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-21 10:40:45 -0300

    calculo das transacoes com reduce()

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,39 +1,56 @@
-import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
-import { SummaryCard, SummaryContainer } from "./styles";
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react';
+import { SummaryCard, SummaryContainer } from './styles';
 import { useContext } from 'react';
-import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => { // acc = accumulator
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  });
 
   return (
     <SummaryContainer>
       <SummaryCard>
         <header>
           <span>Entradas</span>
-          <ArrowCircleUp size={32} color="#00b37e" />
+          <ArrowCircleUp size={32} color='#00b37e' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
         <header>
           <span>Saídas</span>
-          <ArrowCircleDown size={32} color="#f75a68" />
+          <ArrowCircleDown size={32} color='#f75a68' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.outcome}</strong>
       </SummaryCard>
 
-      <SummaryCard variant="green">
+      <SummaryCard variant='green'>
         <header>
           <span>Total</span>
-          <CurrencyDollar size={32} color="#fff" />
+          <CurrencyDollar size={32} color='#fff' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>R$ {summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
-  )
+  );
 }
