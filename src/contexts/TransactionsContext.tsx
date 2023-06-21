912726f33d156194ea3fbd40e19337e9fb3a03ee
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-21 10:08:22 -0300

    Uso de Contexto para transacoes

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, ReactNode, useState, useEffect } from 'react';
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionContextType {
+  transactions: Transaction[];
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType);
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+
+    setTransactions(data);
+    console.log(data);
+  }
+
+  // useEffect não pode ser assíncrono, por isso, para evitarmos cascata de .then, precisamos fazer a função assíncrona separada acima (antes foi feita dentro do useEffect, que também funciona)
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
