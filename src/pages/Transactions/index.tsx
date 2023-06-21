b561d97a8bafa38e035a63f3cc3443271b0352c0
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-21 09:41:15 -0300

    Realizando requisicao HTTP

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { SearchForm } from './components/SearchForm';
@@ -7,7 +8,31 @@ import {
   TransactionsTable,
 } from './styles';
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+
+    setTransactions(data);
+    console.log(data);
+  }
+
+  // useEffect não pode ser assíncrono, por isso, para evitarmos cascata de .then, precisamos fazer a função assíncrona separada acima (antes foi feita dentro do useEffect, que também funciona)
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -18,22 +43,19 @@ export function Transactions() {
 
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+              <td width='50%'>{transaction.description}</td>
               <td>
-                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+                <PriceHighlight variant={transaction.type}>R$ {transaction.price}</PriceHighlight>
               </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
             </tr>
-            <tr>
-              <td>Hamburger</td>
-              <td>
-                <PriceHighlight variant='outcome'>- R$ 59,00</PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+              )
+            })}
+            
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
