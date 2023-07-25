36eb4c4bc5831e398735aade069cc40737df39ea
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-25 18:38:46 -0300

    Filtro das Transações com o Form
    Aula de aplicar o filtro de busca das transações.

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,7 +11,8 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
-}
+  fetchTransactions: (query?: string) => Promise<void>;
+} // Promise por ser assíncrona
 
 export const TransactionsContext = createContext({} as TransactionContextType);
 
@@ -22,21 +23,27 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions');
+  async function fetchTransactions(query?: string) {
+    const url = new URL('http://localhost:3333/transactions');
+
+    if (query) { // tipo um ?q no fim da URL
+      url.searchParams.append('q', query);
+    }
+
+    const response = await fetch(url);
     const data = await response.json();
 
     setTransactions(data);
-    console.log(data);
+    // console.log(data);
   }
 
   // useEffect não pode ser assíncrono, por isso, para evitarmos cascata de .then, precisamos fazer a função assíncrona separada acima (antes foi feita dentro do useEffect, que também funciona)
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
       {children}
     </TransactionsContext.Provider>
   );
