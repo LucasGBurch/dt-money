08bf9b6d6cb7225832bdb1e11ab5fa0118f0f3de
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-07-29 18:59:38 -0300

    Aplicando o useCallback()
    Com anotações a respeito da performance envolvida.

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode, useState, useEffect, useCallback } from 'react';
 import { createContext } from 'use-context-selector';
 import { api } from '../lib/axios';
 
@@ -33,7 +33,7 @@ interface TransactionsProviderProps {
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('/transactions', {
       params: {
         _sort: 'createdAt', // pesquisa no Git do JSON-Server parâmetro de ordenamento
@@ -44,21 +44,24 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
     setTransactions(response.data);
     // console.log(data);
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data;
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data;
 
-    const response = await api.post('transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(), // No Backend real, eles geram este dado. Criamos para não precisar configurar muita cois no JSON-Server
-    });
+      const response = await api.post('transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(), // No Backend real, eles geram este dado. Criamos para não precisar configurar muita cois no JSON-Server
+      });
 
-    setTransactions((state) => [response.data, ...state]);
-  }
+      setTransactions((state) => [response.data, ...state]);
+    },
+    []
+  );
 
   // useEffect não pode ser assíncrono, por isso, para evitarmos cascata de .then, precisamos fazer a função assíncrona separada acima (antes foi feita dentro do useEffect, que também funciona)
   useEffect(() => {
